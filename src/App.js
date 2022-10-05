fbccddc8da89ab88f602b305f92137d83be366a9
Douglas Oliveira <douglasoliveirab11@gmail.com>
2022-10-05 15:21:36 -0300

    Todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+let [lista, setLista] = useState([]);
+let [novoItem, setNovoItem] = useState("")
+
+// useEffect(() =>{
+//   setLista([""])
+// },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+          <input placeholder="Insira uma Tarefa" value={novoItem} onChange={e => setNovoItem(e.target.value)} type="text" />
+          <button onClick={() => adicionarNovoItem()}>Adicionar</button>
+      <ul className="todo-list">
+            {lista.map((item, index) =>(
+            <li className="todo-item" key={index}>
+              {item}
+          <button onClick={() => deletarItem(index)}>
+            Deletar
+            </button>  
+              </li>))}
+      </ul>
     </div>
-  );
+
+);
+      function adicionarNovoItem(){
+        if(novoItem.length <= 0 ){
+          alert("Por favor, digite algo no campo tarefa.")
+          return;
+        }
+
+        let itemIndex = lista.indexOf(novoItem);
+        if(itemIndex >= 0){
+          alert("Você já adicionou esta tarefa.")
+          return;
+        }
+
+
+        setLista([...lista, novoItem]) 
+        setNovoItem("");
+      }
+      function deletarItem(index){
+        let tempArray = [...lista];
+        tempArray.splice(index,1);
+        setLista(tempArray);
+      }
 }
+
 
 export default App;
